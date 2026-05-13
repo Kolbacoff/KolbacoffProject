4df6187bd7437bce8a71b6592474801a910fcb7c
Danila Kolbasenko <danila@example.com>
2026-05-13 03:43:57 +0200

    Remove all files from repository

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://uohtkwtmmdmudhceelzg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_BSs4h9Iip84JrYDaRXJxqg_sGLK2dHs'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
